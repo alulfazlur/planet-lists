369de8cfc31ce8da442bfd456c4645f91384963b
Fazlur Rahman <fazlur0504@gmail.com>
2021-02-23 09:37:04 +0700

    table component

---
+++ src/components/Table.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+
+export default function Table(props) {
+  const { data, viewDetail } = props;
+  return (
+    <div>
+      <div className="shadow overflow-hidden border border-gray-200 sm:rounded-lg m-6">
+        <table className="w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+            <tr>
+              <th className="px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
+
+              <th className="px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Name
+              </th>
+
+              <th className="px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Diameter
+              </th>
+
+              <th className="px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Populations
+              </th>
+              <th className="px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Details
+              </th>
+            </tr>
+          </thead>
+          <tbody className="bg-white text-xs divide-y divide-gray-200">
+            {data.map((planet, index) => (
+              <tr key={index}>
+                <td className="px-2 py-4 whitespace-nowrap"></td>
+                <td className="px-2 py-4 whitespace-nowrap">{planet.name}</td>
+                <td className="px-2 py-4 whitespace-nowrap">
+                  {planet.diameter}
+                </td>
+                <td className="px-2 py-4 whitespace-nowrap">
+                  {planet.population}
+                </td>
+                <td className="px-2 py-4 whitespace-nowrap">
+                  <div className="flex justify-start space-x-1">
+                    <button
+                      onClick={() => viewDetail(planet.url)}
+                      className="border-2 border-indigo-300 
+                    rounded-md p-1 hover:border-indigo-200 transition duration-300"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        className="h-4 w-4 text-indigo-500"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                        />
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
